fbc26941eece9d3392614133b46528400e0beeda
Malakhow-Alexandr <chuk3pc@gmail.com>
2022-12-10 16:03:09 +0200

    5 task done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const resultMarcup = ingredients.map((element) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("item");
+
+  const itemTextEl = document.createElement("p");
+  itemTextEl.textContent = element;
+
+  listItemEl.append(itemTextEl);
+  return listItemEl;
+});
+
+listEl.append(...resultMarcup);
